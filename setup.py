e8468ae9ffcdcc6d2bf54ad664579d3aee655091
MariusWirtz <MariusWirtz2@gmail.com>
2022-08-19 18:36:39 +0200

    Release 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ DESCRIPTION = open('readme.md').read()
 
 setup(
     name="mdxpy",
-    version='0.3',
+    version='0.4',
     maintainer='Marius Wirtz',
     maintainer_email='MWirtz@cubewise.com',
     license="MIT-LICENSE",
